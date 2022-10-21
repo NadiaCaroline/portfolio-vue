c0d31fa7bcb450234baa0e4a21f75bbffff84747
Nadia Caroline Harry Wibowo <carolinenadia314@gmail.com>
2022-10-21 19:34:10 +0700

    portofolio-vue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar app color="black" dark flat class="px-12">
+    <v-btn> <v-icon color="blue" left class="mr-2">fas fa-signature</v-icon>NADIA </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn text @click="scroll('home')" class="text-blue">Home</v-btn>
+    <v-btn text @click="scroll('about')">About</v-btn>
+    <v-btn text @click="scroll('portfolio')">Portfolio</v-btn>
+    <v-btn text @click="scroll('contact')">Contact</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  methods: {
+    scroll(refName) {
+      const element = document.getElementById(refName);
+      element.scrollIntoView({ behavior: "smooth" });
+    },
+  },
+};
+</script>
